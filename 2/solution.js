b48fd1d6952beea0f595dfaa2da9abe1de5b8fd3
Andrew <andyjacuw@gmail.com>
2014-11-21 14:06:36 -0800

    Initial commit, solutions to problems 1 and 2

---
+++ 2/solution.js
@@ -0,0 +1,20 @@
+var fibList = [1, 2];
+
+(function populateFibList() {
+  var nextNum = fibList[fibList.length - 1] + fibList[fibList.length - 2];
+  if (nextNum > 4000000) evenNumSumFinder(fibList);
+  else {
+    fibList.push(nextNum);
+    populateFibList();
+  }
+})();
+
+function evenNumSumFinder(fibList) {
+  var sum = 0;
+  for (var i = 0; i < fibList.length; i++) {
+    if (fibList[i] % 2 === 0) {
+      sum += fibList[i];
+    }
+  }
+  console.log(sum);
+}
